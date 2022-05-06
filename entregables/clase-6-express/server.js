955443a7a800e3c7197455bac235c3cbbbc512bc
fedesouto <federicogsouto@gmail.com>
2022-05-06 15:30:08 -0300

    Update server.js

--- entregables/clase-6-express/server.js
+++ entregables/clase-6-express/server.js
@@ -1,8 +1,12 @@
 const express = require('express')
 const Contenedor = require('../clase-4/Contenedor')
-const productos = new Contenedor('productos.txt')
+const productos = new Contenedor('./clase-6-express/productos.txt')
 
 const app = express()
+
+app.get('/', (req, res) => {
+  res.send('<div><a href="/productos">ver productos</a><br><a href="/productoRandom">ver producto al azar</a></div>')
+})
 
 app.get('/productos', async (req, res) => {
     const data = await productos.getAll()
